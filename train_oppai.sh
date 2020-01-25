11a1f5d1d1640ace8034c1f34f781cc3670562c3
You Gang Kuo <>
2020-01-25 15:57:15 +0800

    ADD SH

---
+++ train_oppai.sh
@@ -0,0 +1,13 @@
+
+
+# for run training oppai
+#
+#
+#
+python run_training.py --num-gpus=1 --data-dir=~/datasets/oppai --config=config-f \
+  --dataset=oppai --mirror-augment=true
+
+
+#
+#
+# python dataset_tool.py create_from_images ~/Desktop/datasets/oppai ~/Desktop/oppai128_20200126
